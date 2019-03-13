4f30162cc484d395d4b417f560f8ced4449082ab
jamsubzero <jamsubzero@gmail.com>
2019-03-13 11:23:32 +0800

    updated conn heroku

--- src/main/java/com/example/demo/controller/SsgResultController.java
+++ src/main/java/com/example/demo/controller/SsgResultController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.SsgResult;
-import com.example.demo.entity.Student;
 import com.example.demo.service.SsgResultServiceImpl;
 
 @RestController
