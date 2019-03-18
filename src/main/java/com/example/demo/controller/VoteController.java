f2ab8f6014e3f35f16f5c13d12228f0d1407801f
jamsubzero <jamsubzero@gmail.com>
2019-03-18 12:01:39 +0800

    added CastBallotResult

--- src/main/java/com/example/demo/controller/VoteController.java
+++ src/main/java/com/example/demo/controller/VoteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.SsgResult;
 import com.example.demo.entity.model.Ballot;
+import com.example.demo.entity.model.CastBallotResult;
 import com.example.demo.entity.model.VoteRequest;
 import com.example.demo.entity.model.VoteResponse;
 import com.example.demo.service.SsgResultService;
@@ -41,7 +42,7 @@ public class VoteController {
 	}
 	
 	@PostMapping("castballot")
-	public String castBallot(@RequestBody Ballot ballot) {
+	public CastBallotResult castBallot(@RequestBody Ballot ballot) {
 		List<SsgResult> ssgResults = new ArrayList<>();
 	    String voterID = ballot.getVoter();
 	    voterServiceImpl.updateVoterStatus(voterServiceImpl.getVoterById(voterID).get());
@@ -59,7 +60,7 @@ public class VoteController {
 	    }
 	    
 	    ssgResultServiceImpl.insertResults(ssgResults);
-		return "success";
+		return new CastBallotResult("Sucess");
 	}
 	
 	@GetMapping("tryCastballot")
---
+++ src/main/java/com/example/demo/entity/model/CastBallotResult.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity.model;
+
+public class CastBallotResult {
+
+	String result;
+
+	public CastBallotResult(String result) {
+		super();
+		this.result = result;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	@Override
+	public String toString() {
+		return "CastBallotResult [result=" + result + "]";
+	}
+	
+	
+	
+	
+	
+}
